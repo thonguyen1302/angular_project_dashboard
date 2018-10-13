ae4800be0a459d7e5990ff7bbc9785ad4ef4056a
Tho Nguyen <nguyentantho1302@gmail.com>
2018-10-12 22:58:09 -0400

    second commit

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ app.controller("projectController", function($scope) {
   $scope.projects = 
   [
     {
-      name: 'Project 1',
-      description: 'Project 1 description'
+        "name": "IBM",
+        "description": "IBM is an American multinational technology and consulting corporation that manufactures and markets computer hardware and software, and offers infrastructure, hosting and consulting services in areas ranging from mainframe computers to nanotechnology."
     },
     {
-      name: 'Project 2',
-      description: 'Project 2 description'
+        "name": "Kids at Hope",
+        "description": "The project was to create an interactive and content rich website that supports students in their efforts to visualize their future by using the neuroscience concept of Time Traveler.  The Time Traveler guides students in seeing and planning for the future. "
     },
     {
-      name: 'Project 3',
-      description: 'Project 3 description'
+        "name": "Levementum, LLC",
+        "description": "The student team worked on a proof of concept to develop a cloud based Business Intelligence Solutions for the Health Care industry tied to CRM.  Editor's note: The result of the MIS student project work is allowing Levementum to enter into a new service practice worth millions in future revenue."
     }
   ];
 
